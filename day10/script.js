32f17e64ea76bfaedd3982581ece1a1d9fd514e6
annapurnachinta <annapurnachinta1234@gmail.com>
2021-05-12 00:01:37 +0530

    added check function

---
+++ day10/script.js
@@ -0,0 +1,6 @@
+let ckeckbox = document.querySelector('.checkbox')
+let input = document.querySelector('input')
+
+function check(){
+    input.checked ? input.checked = false : input.checked = true
+}
