33ddc9c5af27cfccf186fab091325af1a70e440d
annapurnachinta <annapurnachinta1234@gmail.com>
2021-05-24 16:08:26 +0530

    changed percentage to liter

--- day21/script.js
+++ day21/script.js
@@ -5,6 +5,7 @@ let remaine = document.querySelector('.remaine')
 
 let percentage_glass = 12.5
 let remaining = 100
+let total = 2000
 
 small_glass.forEach((glass) =>{
     glass.addEventListener("click", function(){
@@ -15,13 +16,14 @@ small_glass.forEach((glass) =>{
 
 function removing(){
     remaining -= percentage_glass
+    total -= 250
     if(remaining === 0){
         liter.textContent = ''
         percentage.textContent = '100%'
         percentage.style.height = '100%'
         remaine.style.display = 'none'
     }else{
-        liter.textContent = `${remaining}%`
+        liter.textContent = `${total/1000}L`
         percentage.style.height = `${100 - remaining}%`
         percentage.textContent = `${100 - remaining}%`
     }
